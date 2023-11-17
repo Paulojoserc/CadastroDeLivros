e19f68e8f2c36eb11c541b7cccdf7787867875a6
Paulojoserc <pjrc83@gmail.com>
2023-11-17 00:03:00 -0300

    Iniciando o Projeto da UTD

---
+++ src/main/java/br/com/pjtec/livraria/LivrariaUtdApplication.java
@@ -0,0 +1,13 @@
+package br.com.pjtec.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaUtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaUtdApplication.class, args);
+	}
+
+}
