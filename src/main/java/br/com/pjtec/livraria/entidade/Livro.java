8be60485d7fde1256fe3eaa9fc12111a1e4ab68d
Paulojoserc <pjrc83@gmail.com>
2023-11-20 23:50:08 -0300

    entidade Livro

---
+++ src/main/java/br/com/pjtec/livraria/entidade/Livro.java
@@ -0,0 +1,10 @@
+package br.com.pjtec.livraria.entidade;
+
+public class Livro {
+	private long id;
+	private String nome;
+	private String autor;
+	private int quantidade;
+	private double preco;
+	private Enum categoria;
+}
