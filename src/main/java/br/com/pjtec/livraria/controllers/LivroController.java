5a700bc3b35449f3cad8eb8c964f82153cc226cf
Paulojoserc <pjrc83@gmail.com>
2023-11-21 23:52:46 -0300

    Editar

--- src/main/java/br/com/pjtec/livraria/controllers/LivroController.java
+++ src/main/java/br/com/pjtec/livraria/controllers/LivroController.java
@@ -3,6 +3,7 @@ package br.com.pjtec.livraria.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,6 +35,22 @@ public class LivroController {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Livro/listLivros");
 		mv.addObject("listLivros", livroRepositore.findAll());
+		return mv;
+	}
+	
+	@GetMapping("/alterar/{id}")
+	public ModelAndView alterar(@PathVariable("id") Integer id) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Livro/alterar");
+		Livro livro = livroRepositore.findById(id).orElseThrow();
+		mv.addObject("livro",livro);
+		return mv;
+	}
+	@PostMapping("/alterar")
+	public ModelAndView alterar(Livro livro) {
+		ModelAndView mv =  new ModelAndView();
+		livroRepositore.save(livro);
+		mv.setViewName("redirect:/livros-adicionados");
 		return mv;
 	}
 }
